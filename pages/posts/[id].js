8c205ac4be7eb87117b578696fefa7f93a834585
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-20 15:33:36 +0300

    Added: Dynamic Routing

---
+++ pages/posts/[id].js
@@ -0,0 +1,29 @@
+import Layout from '../../components/Layout/Layout';
+import { getPostData, getAllPostsIds } from '../../lib/posts';
+
+export async function getStaticPaths() {
+  const ids = getAllPostsIds();
+  const paths = ids.map((id) => ({
+    params: { id },
+  }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const post = getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export default function Post({ post }) {
+  return (
+    <Layout>
+      <h1>{post.data.title}</h1>
+      <h4>{post.data.date}</h4>
+      <p>{post.content}</p>
+    </Layout>
+  );
+}
