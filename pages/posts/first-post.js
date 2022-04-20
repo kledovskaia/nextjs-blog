195925062f7628d3ebff8cb7eb8066928fdb8a46
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-04-20 12:01:06 +0300

    Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/Layout/Layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -11,6 +12,6 @@ export default function FirstPost() {
       <Link href="/">
         <a>Back to home</a>
       </Link>
-    </>
+    </Layout>
   );
 }
